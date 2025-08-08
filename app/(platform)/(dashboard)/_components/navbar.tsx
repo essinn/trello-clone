3a7cc5f92fd22d053f1e2eb5feb80c3e2e48f1db
essinn <ehsan.raj@hotmail.com>
2025-08-08 02:20:32 +0100

    implement responsive organization dashboard sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,10 +3,12 @@ import { Logo } from "@/components/ui/logo";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import React from "react";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
   return (
     <div className="fixed z-50 top-0 w-full h-14 px-4 border-b shadow-sm bg-white dark:bg-black flex items-center">
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
