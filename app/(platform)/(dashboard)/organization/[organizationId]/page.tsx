f6944fbda3b8cd9c0dd5461156b3217ef1647faa
essinn <ehsan.raj@hotmail.com>
2025-08-10 23:06:28 +0100

    create board popover form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-
-import { prisma } from "@/lib/prisma";
-import Board from "./board";
-import { Form } from "./form";
+import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
 
 export default async function OrganizationIdPage() {
-  const boards = await prisma.board.findMany();
-
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map(board => (
-          <Board key={board.id} id={board.id} title={board.title} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../ui/button";
+import { XIcon } from "lucide-react";
+import { FormInput } from "./form-input";
+import { FormSubmit } from "./form-submit";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/lib/actions/create-board";
+import { toast } from "sonner";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: data => {
+      toast.success("Board created successfully!");
+    },
+    onError: error => {
+      toast.error(error);
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-80 pt-3"
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 dark:text-neutral-400 pb-4">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600 dark:text-neutral-400"
+          >
+            <XIcon className="w-4 h-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
